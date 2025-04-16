58df143f451da8755ede3f0d4cfab7311b7e540e
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-16 17:18:45 +0200

    new group routes

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path("teams/", views.TeamListCreate.as_view(), name="team-list"),
     path("teams/delete/<int:pk>/", views.TeamDelete.as_view(), name="team-delete"),
+    path("groups/", views.TournamentGroupList.as_view(), name="group-list"),
+    path(
+        "groups/bulk/",
+        views.TournamentGroupBulkCreate.as_view(),
+        name="group-bulk-create",
+    ),
 ]
